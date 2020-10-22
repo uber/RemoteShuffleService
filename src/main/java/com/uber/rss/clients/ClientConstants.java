38a6af2b037ca65c7c0647e97405596b449404ae
Mayank Bansal <mabansal@uber.com>
2020-10-21 17:01:24 -0700

    Cleaning up channel manager

--- src/main/java/com/uber/rss/clients/ClientConstants.java
+++ src/main/java/com/uber/rss/clients/ClientConstants.java
@@ -15,5 +15,5 @@
 package com.uber.rss.clients;
 
 public class ClientConstants {
-    public static final long DEFAULT_CONNECTION_IDLE_TIMEOUT_MILLIS = 2 * 60 * 1000;
+    public static final long DEFAULT_CONNECTION_IDLE_TIMEOUT_MILLIS = 3 * 60 * 1000;
 }
