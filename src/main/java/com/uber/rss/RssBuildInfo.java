094333bded438119caedcb58703f0c69ce004f9f
Mayank Bansal <mabansal@uber.com>
2022-08-23 14:46:29 -0700

    Initial commit for Sprak 3.2 for K8

--- src/main/java/com/uber/rss/RssBuildInfo.java
+++ src/main/java/com/uber/rss/RssBuildInfo.java
@@ -1,10 +1,13 @@
 /*
- * Copyright (c) 2020 Uber Technologies, Inc.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,25 +24,24 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class RssBuildInfo {
-    public static String UnknownValue = "<unknown>";
+  public static String UnknownValue = "<unknown>";
 
-    public static String Version;
-    public static String Revision;
+  public static String Version;
+  public static String Revision;
 
-    static {
-        String fileName = "rss-build-info.properties";
-        try (InputStream resourceStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName)) {
-            if (resourceStream == null) {
-                throw new RssException(String.format("Could not find info file: %s", fileName));
-            }
+  static {
+    String fileName = "rss-build-info.properties";
+    try (InputStream resourceStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName)) {
+      if (resourceStream == null) {
+        throw new RssException(String.format("Could not find info file: %s", fileName));
+      }
 
-            Properties props = new Properties();
-            props.load(resourceStream);
-
-            Version = props.getProperty("version", UnknownValue);
-            Revision = props.getProperty("revision", UnknownValue);
-        } catch (IOException e) {
-            throw new RssException(String.format("Failed to open info file: %s", fileName));
-        }
+      Properties props = new Properties();
+      props.load(resourceStream);
+      Version = props.getProperty("version", UnknownValue);
+      Revision = props.getProperty("revision", UnknownValue);
+    } catch (IOException e) {
+      throw new RssException(String.format("Failed to open info file: %s", fileName));
     }
+  }
 }
