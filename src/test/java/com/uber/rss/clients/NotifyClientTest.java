52a0d2460850780820398ef4680905e7ca9884a5
Mayank Bansal <mabansal@uber.com>
2022-09-16 12:36:47 -0700

    Adding multi server write client for K8

--- src/test/java/com/uber/rss/clients/NotifyClientTest.java
+++ src/test/java/com/uber/rss/clients/NotifyClientTest.java
@@ -1,10 +1,13 @@
 /*
- * Copyright (c) 2020 Uber Technologies, Inc.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,39 +24,42 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class NotifyClientTest {
-    private static final Logger logger = LoggerFactory.getLogger(NotifyClientTest.class);
-    
-    @Test
-    public void finishApplicationJob() {
-        TestStreamServer testServer = TestStreamServer.createRunningServer();
+  private static final Logger logger = LoggerFactory.getLogger(NotifyClientTest.class);
 
-        try (NotifyClient client = new NotifyClient("localhost", testServer.getShufflePort(), TestConstants.NETWORK_TIMEOUT, "user1")) {
-            client.connect();
-            // send same request twice to make sure it is still good
-            client.finishApplicationJob("app1", "exec1", 1, "success", null, null);
-            client.finishApplicationJob("app1", "exec1", 1, "success", "", "");
-            // send different request to make sure it is still good
-            client.finishApplicationJob("app1", "exec1", 2, "fail", "RuntimeException", "Exception \nStacktrace");
-        } finally {
-            testServer.shutdown();
-        }
+  @Test
+  public void finishApplicationJob() {
+    TestStreamServer testServer = TestStreamServer.createRunningServer();
+
+    try (NotifyClient client = new NotifyClient("localhost", testServer.getShufflePort(),
+        TestConstants.NETWORK_TIMEOUT, "user1")) {
+      client.connect();
+      // send same request twice to make sure it is still good
+      client.finishApplicationJob("app1", "exec1", 1, "success", null, null);
+      client.finishApplicationJob("app1", "exec1", 1, "success", "", "");
+      // send different request to make sure it is still good
+      client.finishApplicationJob("app1", "exec1", 2, "fail", "RuntimeException",
+          "Exception \nStacktrace");
+    } finally {
+      testServer.shutdown();
     }
+  }
 
-    @Test
-    public void finishApplicationAttempt() {
-        TestStreamServer testServer = TestStreamServer.createRunningServer();
+  @Test
+  public void finishApplicationAttempt() {
+    TestStreamServer testServer = TestStreamServer.createRunningServer();
 
-        try (NotifyClient client = new NotifyClient("localhost", testServer.getShufflePort(), TestConstants.NETWORK_TIMEOUT, "user1")) {
-            client.connect();
-            // send same request twice to make sure it is still good
-            client.finishApplicationAttempt("app1", "exec1");
-            client.finishApplicationAttempt("app1", "exec1");
-            // send different request to make sure it is still good
-            client.finishApplicationAttempt("app1", "exec2");
-            client.finishApplicationAttempt("app2", "exec2");
-        } finally {
-            testServer.shutdown();
-        }
+    try (NotifyClient client = new NotifyClient("localhost", testServer.getShufflePort(),
+        TestConstants.NETWORK_TIMEOUT, "user1")) {
+      client.connect();
+      // send same request twice to make sure it is still good
+      client.finishApplicationAttempt("app1", "exec1");
+      client.finishApplicationAttempt("app1", "exec1");
+      // send different request to make sure it is still good
+      client.finishApplicationAttempt("app1", "exec2");
+      client.finishApplicationAttempt("app2", "exec2");
+    } finally {
+      testServer.shutdown();
     }
+  }
 
 }
